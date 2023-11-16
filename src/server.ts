aee98fb399fd46ca92143d188258e1861ba5fd89
julio cesar <jcmcf2012@gmail.com>
2023-11-16 08:07:59 -0300

    feat: try vercel deploy

--- src/server.ts
+++ src/server.ts
@@ -1,91 +1,91 @@
-import cors from '@fastify/cors'
-import fastifySwagger from '@fastify/swagger'
-import fastifySwaggerUi from '@fastify/swagger-ui'
-import { FastifyReply } from 'fastify'
-import { app } from './app'
-import { env } from './env'
-import { scoreRoutes } from './routes/score'
-import { userRoutes } from './routes/user'
+import cors from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import { FastifyReply } from "fastify";
+import { app } from "./app";
+import { env } from "./env";
+import { scoreRoutes } from "./routes/score";
+import { userRoutes } from "./routes/user";
 
-app.register(cors, { origin: '*' })
+app.register(cors, { origin: "*" });
 
 app.register(fastifySwagger, {
   openapi: {
     info: {
-      title: 'API ScoreSlam',
-      description: 'API Documentation for Programming Challenge',
-      version: '1.0.0',
+      title: "API ScoreSlam",
+      description: "API Documentation for Programming Challenge",
+      version: "1.0.0",
     },
     servers: [
       {
-        url: 'https://second-challenge-backend.onrender.com/',
+        url: "https://second-challenge-backend.onrender.com/",
       },
     ],
     tags: [
-      { name: 'User', description: 'User Routes' },
-      { name: 'Score', description: 'Score Routes' },
+      { name: "User", description: "User Routes" },
+      { name: "Score", description: "Score Routes" },
     ],
     components: {
       schemas: {
         User: {
-          type: 'object',
+          type: "object",
           properties: {
             title: {
-              type: 'string',
-              example: 'User',
+              type: "string",
+              example: "User",
             },
             required: {
-              type: 'array',
-              example: ['name', 'email', 'password'],
+              type: "array",
+              example: ["name", "email", "password"],
               items: {
-                type: 'string',
+                type: "string",
               },
             },
             properties: {
-              type: 'object',
+              type: "object",
               properties: {
                 id: {
-                  type: 'object',
+                  type: "object",
                   properties: {
                     type: {
-                      type: 'string',
-                      example: 'string',
+                      type: "string",
+                      example: "string",
                     },
                   },
                 },
                 name: {
-                  type: 'object',
+                  type: "object",
                   properties: {
                     type: {
-                      type: 'string',
-                      example: 'John Doe',
+                      type: "string",
+                      example: "John Doe",
                     },
                   },
                 },
                 username: {
-                  type: 'object',
+                  type: "object",
                   properties: {
                     type: {
-                      type: 'string',
-                      example: 'johndoe',
+                      type: "string",
+                      example: "johndoe",
                     },
                   },
                 },
                 email: {
-                  type: 'object',
+                  type: "object",
                   properties: {
                     type: {
-                      type: 'string',
-                      example: 'johndoe@outlook.com',
+                      type: "string",
+                      example: "johndoe@outlook.com",
                     },
                   },
                 },
                 password: {
-                  type: 'object',
+                  type: "object",
                   properties: {
                     type: {
-                      type: 'string',
-                      example: '123456',
+                      type: "string",
+                      example: "123456",
                     },
                   },
                 },
@@ -96,43 +96,43 @@ app.register(fastifySwagger, {
       },
     },
   },
-})
+});
 
 app.register(fastifySwaggerUi, {
-  routePrefix: '/documentation',
+  routePrefix: "/documentation",
   uiConfig: {
-    docExpansion: 'list',
+    docExpansion: "list",
     deepLinking: false,
   },
   uiHooks: {
     onRequest: function (request, reply, next) {
-      next()
+      next();
     },
     preHandler: function (request, reply, next) {
-      next()
+      next();
     },
   },
   staticCSP: true,
   transformStaticCSP: (header) => header,
   transformSpecification: (swaggerObject) => {
-    return swaggerObject
+    return swaggerObject;
   },
   transformSpecificationClone: true,
-})
+});
 
 app.register(userRoutes, {
-  prefix: '/user',
-})
+  prefix: "/user",
+});
 
 app.register(scoreRoutes, {
-  prefix: '/nba',
-})
+  prefix: "/nba",
+});
 
 // redireciona para documentação
-app.get('/', (_, reply: FastifyReply) => {
-  reply.redirect(302, '/documentation')
-})
+app.get("/", (_, reply: FastifyReply) => {
+  reply.redirect(302, "/documentation");
+});
 
-app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP Server running!')
-})
+app.listen({ port: env.PORT, host: "0.0.0.0" }).then(() => {
+  console.log("HTTP Server running!");
+});
